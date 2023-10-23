3337822133acaa1eed5ef24c5c668f3175935941
kwangure <86817609+kwangure@users.noreply.github.com>
2023-10-24 00:19:24 +0300

    feat: make content-width and navbar button configurable (#3)

---
+++ .changeset/grumpy-yaks-wash.md
@@ -0,0 +1,5 @@
+---
+"@svelte-thing/components": patch
+---
+
+Make content-width and navbar button configurable
--- packages/components/src/lib/components/shell/navbar.svelte
+++ packages/components/src/lib/components/shell/navbar.svelte
@@ -3,23 +3,27 @@
 	import Icon from '../icon/simple.svelte';
 	import { mdiMenu } from '@mdi/js';
 
+	export let showOpen = true;
+
 	const { elements } = getSidebarContext();
 	const { show } = elements;
 </script>
 
 <nav
-	class="sticky top-0 z-40 col-start-[nav-start] col-end-[nav-end] row-start-1 row-end-2 flex items-center bg-white dark:bg-neutral-800"
+	class="sticky top-0 z-40 col-start-[nav-start] col-end-[nav-end] row-start-1 gap-1 row-end-2 flex items-center bg-white dark:bg-neutral-800"
 >
-	<button
-		class="-ms-4 flex h-12 w-12 touch-manipulation select-none items-center justify-center rounded p-2 lg:hidden"
-		title="Open Menu"
-		aria-label="Open Menu"
-		use:show={{ focus: true }}
-	>
-		<Icon class="w-full" path={mdiMenu} />
-	</button>
+	{#if showOpen}
+		<button
+			class="-ms-4 flex h-12 w-12 touch-manipulation select-none items-center justify-center rounded p-2 lg:hidden"
+			title="Open Menu"
+			aria-label="Open Menu"
+			use:show={{ focus: true }}
+			>
+			<Icon class="w-full" path={mdiMenu} />
+		</button>
+	{/if}
 	<slot />
 </nav>
 <div
-	class="sticky top-[var(--svui-navbar-height)] z-40 col-start-[page-start] col-end-[page-end] row-start-1 row-end-2 self-end border-b border-neutral-300 dark:border-neutral-600"
+	class="sticky top-[var(--st-navbar-height)] z-40 col-start-[page-start] col-end-[page-end] row-start-1 row-end-2 self-end border-b border-neutral-300 dark:border-neutral-600"
 />
