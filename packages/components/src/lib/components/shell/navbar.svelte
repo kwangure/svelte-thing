c4a7a13258b3282cdc20699147f02c7cf17f7a82
kwangure <kaf.wangure@gmail.com>
2023-10-23 22:13:12 +0300

    feat: configure showing navbar open sidebar button

--- packages/components/src/lib/components/shell/navbar.svelte
+++ packages/components/src/lib/components/shell/navbar.svelte
@@ -3,21 +3,25 @@
 	import Icon from '../icon/simple.svelte';
 	import { mdiMenu } from '@mdi/js';
 
+	export let showOpen = true;
+
 	const { elements } = getSidebarContext();
 	const { show } = elements;
 </script>
 
 <nav
-	class="sticky top-0 z-40 col-start-[nav-start] col-end-[nav-end] row-start-1 row-end-2 flex items-center bg-white dark:bg-neutral-800"
+	class="sticky top-0 z-40 col-start-[nav-start] col-end-[nav-end] row-start-1 gap-1 row-end-2 flex items-center bg-white dark:bg-neutral-800"
 >
-	<button
-		class="-ms-4 flex h-12 w-12 touch-manipulation select-none items-center justify-center rounded p-2 lg:hidden"
-		title="Open Menu"
-		aria-label="Open Menu"
-		use:show={{ focus: true }}
-	>
-		<Icon class="w-full" path={mdiMenu} />
-	</button>
+	{#if showOpen}
+		<button
+			class="-ms-4 flex h-12 w-12 touch-manipulation select-none items-center justify-center rounded p-2 lg:hidden"
+			title="Open Menu"
+			aria-label="Open Menu"
+			use:show={{ focus: true }}
+			>
+			<Icon class="w-full" path={mdiMenu} />
+		</button>
+	{/if}
 	<slot />
 </nav>
 <div
