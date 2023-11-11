1a0e87d4c9defce1109c3776d9041cf7be8243e5
kwangure <kaf.wangure@gmail.com>
2023-11-11 15:31:57 +0300

    feat: center icon content

--- packages/components/src/lib/components/icon/link.svelte
+++ packages/components/src/lib/components/icon/link.svelte
@@ -17,7 +17,7 @@
 	$: rel = target === '_blank' ? 'noopener' : undefined;
 </script>
 
-<a {...props} use:action class="p-2 block rounded hover:bg-neutral-200 dark:hover:bg-neutral-700" {href} title={label} {target} {rel}>
+<a {...props} use:action class="p-2 flex items-center justify-center rounded hover:bg-neutral-200 dark:hover:bg-neutral-700" {href} title={label} {target} {rel}>
 	<Icon path={path} />
 	<span class="sr-only">{label}</span>
 </a>
