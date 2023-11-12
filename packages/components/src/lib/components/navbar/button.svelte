8f5f60cdb77a96b5419d7d35fbe0fa339a7e4cea
kwangure <kaf.wangure@gmail.com>
2023-11-12 15:49:39 +0300

    feat: add navbar link and button

---
+++ packages/components/src/lib/components/navbar/button.svelte
@@ -0,0 +1,13 @@
+<script>
+	/** @type {((arg: HTMLElement) => any)} */
+	export let action = () => {};
+	/** @type {any} */
+	export let props = undefined;
+</script>
+
+<button
+	{...props}
+	use:action
+	class="flex items-center gap-3 px-2 sm:px-3 py-1 rounded hover:bg-neutral-200 dark:hover:bg-neutral-700">
+	<slot></slot>
+</button>
--- packages/components/src/lib/components/shell/index.js
+++ packages/components/src/lib/components/shell/index.js
@@ -1,3 +1,2 @@
 export { default as Main } from './main.svelte';
-export { default as Navbar } from './navbar.svelte';
 export { default as Root } from './root.svelte';
