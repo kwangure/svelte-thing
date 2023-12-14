4bf166f651231c3cf2e5c12d9892a67c40592002
kwangure <86817609+kwangure@users.noreply.github.com>
2023-12-14 22:56:00 +0300

    refactor: use shared workspace configs (#8)

--- packages/components/svelte.config.js
+++ packages/components/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +13,11 @@ const config = {
 			 * other packages that depend on this one.
 			 */
 			lib: '/see-svelte-config',
+		},
+		typescript: {
+			config(config) {
+				config.extends = '../../../config/tsconfig.base.json';
+			},
 		},
 	},
 	preprocess: vitePreprocess(),
