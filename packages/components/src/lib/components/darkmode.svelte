7171726e005236c91934f15a006c07a3612f0955
kwangure <kaf.wangure@gmail.com>
2024-01-16 01:37:50 +0300

    feat: load darkmode in head

---
+++ packages/components/src/lib/components/darkmode.svelte
@@ -0,0 +1,8 @@
+<script>
+	/* eslint-disable svelte/no-at-html-tags */
+	import { createDarkmodeScriptTag } from '../creators/darkmode.js';
+</script>
+
+<svelte:head>
+	{@html createDarkmodeScriptTag()}
+</svelte:head>
