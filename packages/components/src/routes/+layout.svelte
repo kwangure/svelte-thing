3534acaf1cb65b4d65033439a380e9b0d4dfb669
kwangure <kaf.wangure@gmail.com>
2024-01-23 00:28:15 +0300

    feat: refine JS syntax highlighting

--- packages/components/src/routes/+layout.svelte
+++ packages/components/src/routes/+layout.svelte
@@ -1,18 +1,23 @@
 <script>
 	import './layout.css';
-	import { Shell, Sidebar } from '../lib/components/index.js';
+	import { Navbar, Shell } from '../lib/components/index.js';
+	import { createDarkModeButton } from '../lib/creators/index.js';
+	import { Darkmode } from '../lib/components/index.js';
+
+	const { elements, state } = createDarkModeButton();
+	const { button } = elements;
+	const { mode } = state;
 </script>
 
+<Darkmode />
+
 <Shell.Root>
-	<Sidebar.Root>
-		<Sidebar.Section title="layout">
-			<Sidebar.Item>
-				<Sidebar.Link href="#test">Shell</Sidebar.Link>
-			</Sidebar.Item>
-			<Sidebar.Item>
-				<Sidebar.Link href="#test">Sidebar</Sidebar.Link>
-			</Sidebar.Item>
-		</Sidebar.Section>
-	</Sidebar.Root>
+	<Navbar.Root>
+		<div class="ml-auto">
+			<Navbar.Button action={button}>
+				Switch to {$mode === 'dark' ? 'Light' : 'Dark'} mode
+			</Navbar.Button>
+		</div>
+	</Navbar.Root>
 	<slot />
 </Shell.Root>
