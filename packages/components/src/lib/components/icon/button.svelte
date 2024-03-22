6e4536c6019ceddee8dff7d25096a76dff265211
kwangure <kaf.wangure@gmail.com>
2024-03-22 19:50:52 +0300

    feat: untailwind navbar

--- packages/components/src/lib/components/icon/button.svelte
+++ packages/components/src/lib/components/icon/button.svelte
@@ -1,4 +1,5 @@
 <script>
+	import '../../css/size.css';
 	import Icon from './simple.svelte';
 
 	/** @type {(node: HTMLElement) => any} */
@@ -11,7 +12,23 @@
 	export let path;
 </script>
 
-<button {...props} use:action class="p-2 flex items-center justify-center rounded hover:bg-neutral-200 dark:hover:bg-neutral-700" title={label}>
-	<Icon path={path} />
+<button {...props} use:action title={label}>
+	<Icon {path} />
 	<span class="sr-only">{label}</span>
 </button>
+
+<style>
+	button {
+		align-items: center;
+		border-radius: var(--st-size-1);
+		display: flex;
+		justify-content: center;
+		padding: var(--st-size-2);
+	}
+	button:hover {
+		--_background-color: var(--st-color-neutral-200);
+		--_background-color-dark: var(--st-color-preference)
+			var(--st-color-neutral-700);
+		background-color: var(--_background-color-dark, var(--_background-color));
+	}
+</style>
--- packages/components/src/lib/components/icon/simple.svelte
+++ packages/components/src/lib/components/icon/simple.svelte
@@ -1,18 +1,22 @@
 <script>
+	import '../../css/size.css';
+
 	/** @type {boolean} */
 	export let ariaHidden = true;
 	/** @type {string} */
 	export let path;
-
-	/** @type {string | undefined} */
-	let className = undefined;
-	export { className as class };
 </script>
 
-<svg
-	aria-hidden={ariaHidden}
-	viewBox="0 0 24 24"
-	class={className ?? 'h-5 w-5'}
->
-	<path class="fill-current" d={path} />
+<svg aria-hidden={ariaHidden} viewBox="0 0 24 24">
+	<path d={path} />
 </svg>
+
+<style>
+	svg {
+		height: var(--st-icon-height, var(--st-size-5));
+		width: var(--st-icon-width, var(--st-size-5));
+	}
+	path {
+		fill: var(--st-icon-fill, currentColor);
+	}
+</style>
