d1060d6ced0ef0dbe7a13627deee96f0358cf356
kwangure <kaf.wangure@gmail.com>
2024-03-30 22:00:23 +0300

    feat: remove remaining tailwind styles

--- packages/components/src/lib/components/sidebar/section.svelte
+++ packages/components/src/lib/components/sidebar/section.svelte
@@ -1,13 +1,36 @@
 <script>
+	import '../../css/color-preference.css';
+	import '../../css/color.css';
+	import '../../css/size.css';
 	/** @type {string} */
 	export let title;
 </script>
 
-<h5
-	class="sticky top-0 whitespace-nowrap bg-white pb-1 pt-2 text-xs font-semibold uppercase text-neutral-900 dark:bg-neutral-800 dark:text-neutral-400"
->
-	{title}
-</h5>
-<ul class="flex flex-col gap-1">
-	<slot />
-</ul>
+<h5>{title}</h5>
+<ul><slot /></ul>
+
+<style>
+	h5 {
+		--_background-color: var(--st-color-white);
+		--_background-color-dar: var(--st-color-preference-dark)
+			var(--st-color-neutral-800);
+		background-color: var(--_background-color-dark, var(--_background-color));
+		--_color: var(--st-color-neutral-900);
+		--_color-dark: var(--st-color-preference-dark) var(--st-color-neutral-400);
+		color: var(--_color-dark, var(--_color));
+		font-size: var(--st-size-3);
+		font-weight: 600;
+		line-height: var(--st-size-4);
+		padding-block-end: var(--st-size-1);
+		padding-block-start: var(--st-size-2);
+		position: sticky;
+		text-transform: uppercase;
+		top: 0px;
+		white-space: nowrap;
+	}
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: var(--st-size-1);
+	}
+</style>
