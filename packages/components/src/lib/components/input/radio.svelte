25baa7c19be0c62039db488062b281d1604cbf88
kwangure <kaf.wangure@gmail.com>
2024-04-05 20:37:32 +0300

    feat: add name to inputs

--- packages/components/src/lib/components/input/radio.svelte
+++ packages/components/src/lib/components/input/radio.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	export let checked: boolean = false;
+	export let name: string | undefined = undefined;
 	export let value: string;
 	/* eslint-disable-next-line no-unused-vars, @typescript-eslint/no-explicit-any */
 	export let onchange: ((...args: any[]) => any) | undefined = undefined;
 </script>
 
 <label>
-	<input type="radio" {checked} {value} on:change={onchange} />
+	<input type="radio" {checked} {name} {value} on:change={onchange} />
 	<slot />
 </label>
 
