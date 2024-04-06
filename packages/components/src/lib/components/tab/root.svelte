8e1e45d9d85f64dc789a56eda0221fb40a89efef
kwangure <kaf.wangure@gmail.com>
2024-04-06 03:06:38 +0300

    feat: add tab component

---
+++ packages/components/src/lib/components/tab/root.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { createTabsContext } from './tab';
+
+	const { elements } = createTabsContext();
+	const { list, root } = elements;
+</script>
+
+<div class="root" {...$root} use:root>
+	<div class="triggers" {...$list} use:list>
+		<slot name="trigger" />
+	</div>
+	<slot />
+</div>
+
+<style>
+	.root {
+		display: flex;
+		flex-direction: column;
+		gap: var(--st-size-2);
+	}
+	.triggers {
+		gap: var(--st-size-2);
+		display: flex;
+	}
+</style>
