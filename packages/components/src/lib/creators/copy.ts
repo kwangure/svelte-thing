09a41f76db1f24a7a237e7d5dc55426e4e62c26d
kwangure <kaf.wangure@gmail.com>
2024-05-25 18:45:31 +0300

    feat: rework copying in code blocks

---
+++ packages/components/src/lib/creators/copy.ts
@@ -0,0 +1,40 @@
+import { get, writable } from 'svelte/store';
+
+export type LineInterval = [number, number];
+
+interface CopierOptions {
+	text: string | undefined;
+}
+
+export function createCopier(options: CopierOptions) {
+	const textToCopy = writable<string | undefined>(options.text);
+	const isCopied = writable(false);
+
+	let copyTimeout: ReturnType<typeof setTimeout>;
+	function copyText() {
+		if (typeof navigator === 'undefined') return;
+		const text = get(textToCopy);
+		if (!text) return;
+
+		navigator.clipboard.writeText(text);
+		isCopied.set(true);
+		clearTimeout(copyTimeout);
+		copyTimeout = setTimeout(() => isCopied.set(false), 2000);
+	}
+
+	function button(node: HTMLElement) {
+		node.addEventListener('click', copyText);
+		return {
+			destroy() {
+				clearTimeout(copyTimeout);
+				node.removeEventListener('click', copyText);
+			},
+		};
+	}
+
+	return {
+		elements: { button },
+		inputs: { textToCopy },
+		outputs: { isCopied },
+	};
+}
