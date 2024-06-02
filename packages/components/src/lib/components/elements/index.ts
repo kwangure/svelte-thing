02a98781b25cc8008558d07c4e395ce0162ca81a
kwangure <kaf.wangure@gmail.com>
2024-06-02 17:26:15 +0300

    feat: separate element and markdown of image

--- packages/components/src/lib/components/elements/index.ts
+++ packages/components/src/lib/components/elements/index.ts
@@ -2,3 +2,4 @@ export { default as Blockquote } from './blockquote.svelte';
 export { default as Code } from './code.svelte';
 export { default as Emphasis } from './emphasis.svelte';
 export { default as Heading } from './heading.svelte';
+export { default as Image } from './image.svelte';
