ed84d85f77e2c8f9b2521f1ca39ffaab084bb0f6
kwangure <kaf.wangure@gmail.com>
2024-06-02 17:27:19 +0300

    refactor: use TS for HTML

--- packages/components/src/lib/components/markdown/html.svelte
+++ packages/components/src/lib/components/markdown/html.svelte
@@ -1,6 +1,7 @@
-<script>
-	/** @type {import('mdast').HTML} */
-	export let node;
+<script lang="ts">
+	/* eslint-disable svelte/no-at-html-tags */
+	import type { Html } from 'mdast';
+	export let node: Html;
 </script>
 
 {@html node.value}
