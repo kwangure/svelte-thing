37951e1e23e60a0a9e62bee617295e1a5f6db5c8
kwangure <kaf.wangure@gmail.com>
2024-06-02 17:15:39 +0300

    feat: separate element and markdown of code

--- packages/components/src/lib/components/elements/index.ts
+++ packages/components/src/lib/components/elements/index.ts
@@ -1,3 +1,4 @@
 export { default as Blockquote } from './blockquote.svelte';
+export { default as Code } from './code.svelte';
 export { default as Emphasis } from './emphasis.svelte';
 export { default as Heading } from './heading.svelte';
