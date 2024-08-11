3dfd442458b4e8beb947f7ecaf89a8bf6defa1c1
kwangure <kaf.wangure@gmail.com>
2024-08-11 14:41:27 +0300

    refactor: remove unused code

---
+++ .changeset/cold-paws-destroy.md
@@ -0,0 +1,5 @@
+---
+"@svelte-thing/components": patch
+---
+
+Remove unused `createEventForwarder`
--- packages/components/src/lib/creators/index.js
+++ packages/components/src/lib/creators/index.js
@@ -1,3 +1,2 @@
 export * from './copy.js';
 export * from './darkmode.js';
-export * from './forward.js';
