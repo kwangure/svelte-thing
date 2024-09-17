d566bb83db2702ba6de9be4a94afd48ac9ce4c39
kwangure <kaf.wangure@gmail.com>
2024-09-17 18:17:39 +0300

    feat: support 128 key events

--- packages/dom-event/src/keys.ts
+++ packages/dom-event/src/keys.ts
@@ -1,29 +1,46 @@
 export const Mod = {
-	Alt: 1 << 0,
-	Control: 1 << 1,
-	Shift: 1 << 2,
+	Alt: 0,
+	Control: 1,
+	Shift: 2,
 } as const;
 
 export const Keys = {
-	ArrowDown: 1 << 3,
-	Down: 1 << 3, // Legacy alias for 'ArrowDown'
-	ArrowUp: 1 << 4,
-	Up: 1 << 4, // Legacy alias for 'ArrowUp'
-	Backspace: 1 << 5,
-	Delete: 1 << 6,
-	End: 1 << 7,
-	Enter: 1 << 8,
-	Escape: 1 << 9,
-	Esc: 1 << 9, // Legacy alias for 'Escape'
-	Home: 1 << 10,
-	Tab: 1 << 11,
+	ArrowDown: 3,
+	Down: 3, // Legacy alias for 'ArrowDown'
+	ArrowUp: 4,
+	Up: 4, // Legacy alias for 'ArrowUp'
+	Backspace: 5,
+	Delete: 6,
+	End: 7,
+	Enter: 8,
+	Escape: 9,
+	Esc: 9, // Legacy alias for 'Escape'
+	Home: 10,
+	Tab: 11,
+	// ... more keys can be added here up to 127
 } as const;
 
-export function eventToKeys(event: KeyboardEvent): number {
-	return (
-		(event.altKey ? Mod.Alt : 0) |
-		(event.ctrlKey ? Mod.Control : 0) |
-		(event.shiftKey ? Mod.Shift : 0) |
-		(Keys[event.key as keyof typeof Keys] || 0)
-	);
+export function eventToKeys(event: KeyboardEvent): string {
+	const result = new Uint32Array(4); // 4 * 32 = 128 bits
+
+	result[0] |=
+		((event.altKey as unknown as number | 0) << Mod.Alt) |
+		((event.ctrlKey as unknown as number | 0) << Mod.Control) |
+		((event.shiftKey as unknown as number | 0) << Mod.Shift);
+
+	const keyCode = Keys[event.key as keyof typeof Keys] | 0;
+	const index = keyCode >>> 5; // Divide by 32 to get the array index
+	const bitPosition = keyCode & 31; // Modulo 32 to get the bit position
+	result[index] |= 1 << bitPosition;
+
+	let str = '';
+	for (let i = 0; i < result.length; i++) {
+		if (result[i] !== 0 || str !== '') {
+			/* !(result[i] == 0 && str == '') */ str += result[i]
+				.toString(16)
+				.padStart(8, '0');
+		}
+	}
+
+	return str;
 }
