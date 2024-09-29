04e7cc1bd1458f1050032cf74c4d7edafae72940
kwangure <kaf.wangure@gmail.com>
2024-09-29 16:31:37 +0300

    feat: add building workspace

---
+++ sites/workspace/svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$collections: './.collections/collections/',
+			$routes: './src/routes/',
+		},
+		typescript: {
+			config(config) {
+				config.extends = '../../../config/tsconfig.base.json';
+			},
+		},
+	},
+};
+
+export default config;
