d2ab66173af6e159dad778884d7cc2bee7279760
kwangure <kaf.wangure@gmail.com>
2024-10-02 11:49:10 +0300

    feat: add isClickInside to dom-event

--- packages/dom-event/src/utils.ts
+++ packages/dom-event/src/utils.ts
@@ -2,3 +2,18 @@ export function cancelEvent(event: Event) {
 	event.stopPropagation();
 	event.preventDefault();
 }
+
+export function isClickInside(
+	event: Event | MouseEvent,
+	dialog: HTMLDialogElement,
+) {
+	if (!('clientY' in event)) return false;
+	const rect = dialog.getBoundingClientRect();
+	if (rect.width === 0 || rect.height === 0) return false;
+	return (
+		rect.top <= event.clientY &&
+		event.clientY <= rect.bottom &&
+		rect.left <= event.clientX &&
+		event.clientX <= rect.right
+	);
+}
