9d1bf8890f33a6d9f174685905b71deccb79979f
kwangure <kaf.wangure@gmail.com>
2024-10-02 20:45:32 +0300

    feat: dispatch a MouseEvent for onclickoutside

--- packages/components/src/actions/onclickoutside.ts
+++ packages/components/src/actions/onclickoutside.ts
@@ -3,7 +3,7 @@ import { on } from 'svelte/events';
 export function onclickoutside(node: HTMLElement) {
 	const destroy = on(document, 'click', (event) => {
 		if (!node.contains(event.target as Node)) {
-			node.dispatchEvent(new Event('clickoutside', event));
+			node.dispatchEvent(new MouseEvent('clickoutside', event));
 		}
 	});
 
