a6f63c2b698361137c316c41b8f4c2010117594b
kwangure <kaf.wangure@gmail.com>
2024-10-05 23:34:51 +0300

    refactor: remove svelte kit from components

--- packages/components/src/lezer-bash.d.ts
+++
@@ -1,6 +0,0 @@
-// TODO: Delete once https://github.com/withfig/lezer-bash/pull/2 is released 🙄
-declare module '@fig/lezer-bash' {
-	import { LRParser } from '@lezer/lr';
-
-	export const parser: LRParser;
-}
