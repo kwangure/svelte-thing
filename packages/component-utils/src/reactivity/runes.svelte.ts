f8380663e8d126e1fd1280b9698d0f67c544eff8
kwangure <kaf.wangure@gmail.com>
2024-10-13 18:50:00 +0300

    feat: move `createKeyedWatcher` to component-utils

--- packages/component-utils/src/reactivity/runes.svelte.ts
+++ packages/component-utils/src/reactivity/runes.svelte.ts
@@ -1,4 +1,4 @@
-import { untrack } from 'svelte';
+import { tick, untrack } from 'svelte';
 
 /**
  * Runs the provided `fn` function after the signals in `read` have changed a specified
@@ -48,4 +48,78 @@ export function watch<T>(read: () => T, fn: (value: T) => void | (() => void)) {
 		const value = read();
 		untrack(() => fn(value));
 	});
+}
+
+export type KeyedWatcherSetup = () => KeyedWatcherCleanup;
+
+export type KeyedWatcherCleanup = (() => unknown) | void | undefined;
+
+/**
+ * Creates a keyed watcher that runs a setup function after the first listener
+ * of a signal reads it and calls the cleanup function when no more watchers
+ * are listening.
+ *
+ * @example
+ * export function makeClock() {
+ *   let date = $state<Date>();
+ *
+ *   const watcher = createKeyedWatcher();
+ *
+ *   // Setup function used as key to track how many watchers are listening to `date`
+ *   function initializeExpensiveClock() {
+ *     const intervalId = setInterval(() => (date = new Date()), 1000);
+ *     const cleanupWhenNoOneIsListening = () => clearInterval(intervalId);
+ *     return cleanupWhenNoOneIsListening;
+ *   }
+ *
+ *   return {
+ *     get date() {
+ *       watcher.watch(initializeExpensiveClock);
+ *       return date;
+ *     },
+ *     // ...add more getter properties with watchers here
+ *     // You can use the same `initializeExpensiveClock` as the watch key if
+ *     // they rely on `date` or a different function managing a different resource
+ *   };
+ * }
+ */
+export function createKeyedWatcher() {
+	const watchers = new Map<
+		KeyedWatcherSetup,
+		[number, KeyedWatcherCleanup]
+	>();
+
+	return {
+		/**
+		 * Registers a setup function to be watched. If the setup function is
+		 * already being watched, it increases the reference count. When all
+		 * references are removed, the cleanup function is called.
+		 *
+		 * @param setup - A function that sets up the watcher and returns an
+		 * optional cleanup function.
+		 */
+		watch(setup: KeyedWatcherSetup) {
+			if ($effect.tracking()) {
+				$effect(() => {
+					let entry = watchers.get(setup);
+					if (!entry) {
+						const cleanup = untrack(setup);
+						entry = [0, cleanup];
+						watchers.set(setup, entry);
+					}
+					entry[0]++;
+
+					return () => {
+						tick().then(() => {
+							entry[0]--;
+							if (entry[0] === 0) {
+								entry[1]?.(); // Run cleanup
+								watchers.delete(setup);
+							}
+						});
+					};
+				});
+			}
+		},
+	};
 }
