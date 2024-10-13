1d681cdcf1ba43fa99f89930b56466ae9f9d4963
kwangure <kaf.wangure@gmail.com>
2024-10-13 17:26:42 +0300

    feat: add `watch` rune

--- packages/component-utils/src/reactivity/proxy.ts
+++ packages/component-utils/src/reactivity/proxy.ts
@@ -14,6 +14,18 @@ const handler = {
 
 proxy(new Proxy({}, handler));
 
+/**
+ * Retrieves the value from a proxy created by a Svelte rune such as `$state`
+ * or `$derived`. If the value is not a signal, it returns the original value
+ * untouched.
+ *
+ * @example
+ * const data = { foo: 'bar' }
+ * const state = $state(data);
+ * console.log(data === state) // false
+ * console.log(data === getProxiedValue(state)) // true
+ * console.log(getProxiedValue(19)) // 19
+ */
 export function getProxiedValue<T>(value: T) {
 	if (value !== null && typeof value === 'object' && STATE_SYMBOL in value) {
 		// @ts-expect-error signal
@@ -23,6 +35,19 @@ export function getProxiedValue<T>(value: T) {
 	return value;
 }
 
+/**
+ * Compares two values using `Object.is`, taking into account possible proxied
+ * signals
+ *
+ * @param {unknown} a - The first value to compare.
+ * @param {unknown} b - The second value to compare.
+ *
+ * @example
+ * const data = { foo: 'bar' }
+ * const state = $state(data);
+ * console.log(Object.is(data, state)); // false
+ * console.log(stateIs(data, state)) // true
+ */
 export function stateIs(a: unknown, b: unknown) {
 	return Object.is(getProxiedValue(a), getProxiedValue(b));
 }
--- packages/component-utils/src/reactivity/runes.svelte.ts
+++ packages/component-utils/src/reactivity/runes.svelte.ts
@@ -1,5 +1,20 @@
 import { untrack } from 'svelte';
 
+/**
+ * Runs the provided `fn` function after the signals in `read` have changed a specified
+ * number of times.
+ *
+ * @example
+ * skipEffect(() => someValue, (value) => {
+ *   console.log(value); // Will run the effect after `someValue` changes twice.
+ * }, 2);
+ *
+ * @param read - A function that returns a reactive value to be passed to `fn`.
+ * @param fn - A function that takes the result of `read` and executes side
+ * effects. It can optionally return a cleanup function.
+ * @param [skipCount=1] - The number of times the read value will change before
+ * `fn` is executed. Defaults to 1.
+ */
 export function skipEffect<T>(
 	read: () => T,
 	fn: (value: T) => void | (() => void),
@@ -10,5 +25,27 @@ export function skipEffect<T>(
 		const value = read();
 		if (remainingSkips-- > 0) return;
 		return untrack(() => fn(value));
+	});
+}
+
+/**
+ * Similar to `$effect`, but with explicit opt-in to tracking via the `read`
+ * function. Signals in `read` trigger reruns of `fn`, but signals in `fn` will
+ * not trigger reruns of `fn`.
+ *
+ * @example
+ * let unwatchedSignal = $state('foo')
+ * watch(() => someValue, (value) => {
+ *   console.log(value, unwatchedSignal); // Only logs when `someValue` changes.
+ * });
+ *
+ * @param read - A function that returns a reactive value to be watched.
+ * @param fn - A function that is executed when the value changes. It can
+ * optionally return a cleanup function.
+ */
+export function watch<T>(read: () => T, fn: (value: T) => void | (() => void)) {
+	$effect(() => {
+		const value = read();
+		untrack(() => fn(value));
 	});
 }
