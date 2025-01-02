555ae19ca7787a97cce0e2f85497b5aae0514586
kwangure <kaf.wangure@gmail.com>
2025-01-02 17:12:25 +0300

    feat: add `onclickoutsiderect` action

--- packages/rune-components/src/actions/onclickoutside.ts
+++ packages/rune-components/src/actions/onclickoutside.ts
@@ -9,3 +9,24 @@ export function onclickoutside(node: HTMLElement) {
 
 	return { destroy };
 }
+
+export function onclickoutsiderect(node: HTMLElement) {
+	const destroy = on(document, 'click', (event) => {
+		if (!isClickInside(event, node)) {
+			node.dispatchEvent(new MouseEvent('clickoutsiderect', event));
+		}
+	});
+
+	return { destroy };
+}
+
+function isClickInside(event: MouseEvent, element: HTMLElement) {
+	const rect = element.getBoundingClientRect();
+	if (rect.width === 0 || rect.height === 0) return false;
+	return (
+		rect.top <= event.clientY &&
+		event.clientY <= rect.bottom &&
+		rect.left <= event.clientX &&
+		event.clientX <= rect.right
+	);
+}
