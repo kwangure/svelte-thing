91652ed73798127d6615da98688f844ce24e2c0d
kwangure <86817609+kwangure@users.noreply.github.com>
2025-01-11 17:15:31 +0300

    feat: add app css (#67)

--- packages/css/src/base/index.js
+++ packages/css/src/base/index.js
@@ -2,6 +2,7 @@ import './a11y.css';
 import './breakpoint.css';
 import './color-preference.css';
 import './color.css';
+import './app.css';
 import './motion.css';
 import './preflight.css';
 import './size.css';
